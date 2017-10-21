ac4ae2e4777ad06d22e19b4874067ce5eaa5cbd5
Hosamov <hosamov@hotmail.com>
2017-10-21 11:34:46 -0700

    Add files via upload

---
+++ 00-Arduino Mastery Projects/Project1(ControlledBlinking)/CylonLights_Sketch1/CylonLights_Sketch1.ino
@@ -0,0 +1,67 @@
+
+// Set positions of LED pin(s):
+#define ledPin1 8
+#define ledPin2 9
+#define ledPin3 10
+#define ledPin4 11
+#define ledPin5 12
+#define ledPin6 13
+
+// Setup input pin(s):
+#define potPin 0
+
+void setup() {
+  // Setup LED pins as outputs
+  pinMode(ledPin1,OUTPUT);
+  pinMode(ledPin2,OUTPUT);
+  pinMode(ledPin3,OUTPUT);
+  pinMode(ledPin4,OUTPUT);
+  pinMode(ledPin5,OUTPUT);
+  pinMode(ledPin6,OUTPUT);
+}
+
+void loop() {
+  int potVal = analogRead(potPin);        //Define variable for potVal
+  int potMap = map(potVal,0,1024,0,255);  // Map the values in order to have more control over LED speed
+  digitalWrite(ledPin1,HIGH);
+  delay(potMap);
+  digitalWrite(ledPin1,LOW);
+  delay(potMap);
+  digitalWrite(ledPin2,HIGH);
+  delay(potMap);
+  digitalWrite(ledPin2,LOW);
+  delay(potMap);
+  digitalWrite(ledPin3,HIGH);
+  delay(potMap);
+  digitalWrite(ledPin3,LOW);
+  delay(potMap);
+  digitalWrite(ledPin4,HIGH);
+  delay(potMap);
+  digitalWrite(ledPin4,LOW);
+  delay(potMap);
+  digitalWrite(ledPin5,HIGH);
+  delay(potMap);
+  digitalWrite(ledPin5,LOW);
+  delay(potMap);
+  digitalWrite(ledPin6,HIGH);
+  delay(potMap);
+  digitalWrite(ledPin6,LOW);
+  delay(potMap);
+  digitalWrite(ledPin5,HIGH);
+  delay(potMap);
+  digitalWrite(ledPin5,LOW);
+  delay(potMap);
+  digitalWrite(ledPin4,HIGH);
+  delay(potMap);
+  digitalWrite(ledPin4,LOW);
+  delay(potMap);
+  digitalWrite(ledPin3,HIGH);
+  delay(potMap);
+  digitalWrite(ledPin3,LOW);
+  delay(potMap);
+  digitalWrite(ledPin2,HIGH);
+  delay(potMap);
+  digitalWrite(ledPin2,LOW);
+  delay(potMap);
+
+}
---
+++ 00-Arduino Mastery Projects/Project1(ControlledBlinking)/CylonLights_Sketch2/CylonLights_Sketch2.ino/CylonLights_Sketch2.ino.ino
@@ -0,0 +1,56 @@
+// 10/21/2017, by: Hosamov
+// "Cylon Lights"
+
+// Setup input pin(s):
+#define potPin 0
+
+void setup() {
+  // Setup LED pins as outputs:
+  // LEDs: 1-6, Digital pins: 8-13
+  pinMode(8, OUTPUT);  
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  bool trigger = 1;
+
+  // Have LEDs shift to right
+  while (trigger == true) {
+    for (int cylon = 0; cylon < 5; cylon++) {
+      int cylonMap = map(cylon, 0, 5, 8, 13);    // In this while loop, map cylon with pin values
+      int potVal = analogRead(potPin);           // Define variable for potVal
+      int potMap = map(potVal, 0, 1024, 0, 255); // In this while loop, map actual values with potVal
+      digitalWrite(cylonMap, HIGH);
+      delay(potMap);
+      digitalWrite(cylonMap, LOW);
+      delay(potMap);
+
+      if (cylon == 4) {       // If cylon reaches 5th LED... 
+        trigger = 0;          // Set trigger to false, run next while loop
+        break;                // break from this while loop
+      }
+    }
+    // Now have LEDs shift back to the left
+    while (trigger == false) {
+      for (int cylon = 5; cylon <= 5; cylon--) {
+        int cylonMap = map(cylon, 0, 5, 8, 13);      // In this while loop, map cylon to pin values   
+        int potVal = analogRead(potPin);             // Define variable for potVal
+        int potMap = map(potVal, 0, 1024, 0, 255);   // In this while loop, map actual values to potVal
+        digitalWrite(cylonMap, HIGH);
+        delay(potMap);
+        digitalWrite(cylonMap, LOW);
+        delay(potMap);
+
+        if (cylon == 1) {      // If cylon reaches 1st LED...
+          trigger = 1;         // Set trigger to true, run next while loop
+          break;               // break from this while loop
+        }
+      }
+    }
+  }
+}
+
