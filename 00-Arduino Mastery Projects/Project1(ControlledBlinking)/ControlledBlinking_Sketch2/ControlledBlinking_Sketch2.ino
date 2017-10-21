ccaa4a3a5f2fae18812d6ca0df55f186ed54ed34
Hosamov <hosamov@hotmail.com>
2017-10-20 21:34:01 -0700

    Add files via upload

---
+++ 00-Arduino Mastery Projects/Project1(ControlledBlinking)/ControlledBlinking_Sketch2/ControlledBlinking_Sketch2.ino
@@ -0,0 +1,16 @@
+#define ledPin 13
+#define potPin 0
+
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  int potVal = analogRead(potPin);
+  int potMapped = map(potVal,0,512,0,255);
+  digitalWrite(ledPin, HIGH);   
+  delay(potMapped);                       
+  digitalWrite(ledPin, LOW);    
+  delay(potMapped);     
+}                  
+
